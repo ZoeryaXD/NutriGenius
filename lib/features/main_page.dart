79059793350ded3c025ceb40e6f6308b17c4822b
Royhan <royhanfirdaus12345@gmail.com>
2025-12-14 18:27:50 +0700

    Tambah Tampilan Menu Bar Kosongan

---
+++ lib/features/main_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:nutrigenius/features/dashboard/pages/dashboard_page.dart';
+import 'package:nutrigenius/features/history/presentation/pages/history_page.dart';
+import 'package:nutrigenius/features/notification/pages/notification_page.dart';
+import 'package:nutrigenius/features/profile/pages/profile_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    DashboardPage(),
+    HistoryPage(),
+    NotificationPage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.spa_outlined),
+            activeIcon: Icon(Icons.spa),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long_outlined),
+            activeIcon: Icon(Icons.receipt_long),
+            label: 'History Log',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_none),
+            activeIcon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
